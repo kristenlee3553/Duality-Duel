73f0dee2cbdb3ddc1fd606424bcf72a373c89fd8
Kristen Lee <kristen.lee.3553@gmail.com>
2025-01-25 13:25:38 -0500

    image over text

--- src/Routes/EgoChoices.js
+++ src/Routes/EgoChoices.js
@@ -1,9 +1,21 @@
+import SceneBackground from '../Images/SceneText.png'
+import "../Styles/EgoChoices.css";
+
 function EgoChoices() {
 return (
     <div>
-        <h1>
-            Hi
-        </h1>
+        {/* Header Text*/}
+        <div class= "">
+            <div class="image-overlay">
+                <img src={SceneBackground} class="img-fluid" alt='Scene Text Background'></img>
+                <div class="overlay-text"> 
+                    <h1>Text Over the image(Responsive image).</h1> 
+                </div> 
+            </div>
+        </div>
+        <div class = "blue">
+            
+        </div>
     </div>
   );
 }
