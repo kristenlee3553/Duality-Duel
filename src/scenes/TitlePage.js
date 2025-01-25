ab8dee6556636b8dc422ed1d2722b07ca0500388
usern4 <ciciw8888@gmail.com>
2025-01-25 12:03:09 -0500

    Added a placeholder image

---
+++ src/scenes/TitlePage.js
@@ -0,0 +1,11 @@
+// Import the CSS file here
+
+function TitlePage() {
+    return (
+        <div>
+            <img src="https://preview.redd.it/hand-over-your-silliest-cat-memes-v0-665xjvlhb4yc1.jpeg?width=640&crop=smart&auto=webp&s=d857719af329d11425f874716ff1ec6f0818016e" alt="silly cat"></img>
+        </div>
+    )
+}
+
+export default TitlePage;
