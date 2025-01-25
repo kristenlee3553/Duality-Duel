f95cf9b050306d460ded9b8c66a4ba198e3d44c0
Amanda Li <yuamandali2004@gmail.com>
2025-01-25 14:47:36 -0500

    homepage done

---
+++ src/Routes/Homepage.js
@@ -0,0 +1,16 @@
+import "../Styles/Homepage.css";
+
+function Homepage() {
+    return (
+        <div className="homepage">
+            <div className="buttonsContainer">
+                <button className="button">Play</button>
+                <button className="button">Settings</button>
+                <button className="button">Credits</button>
+                <button className="button">Exit</button>
+            </div>
+        </div>
+    );
+}
+
+export default Homepage;
