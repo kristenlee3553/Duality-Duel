d9bf88fd65d490c59ee99c7a1cf288ec323b7b6c
Kristen Lee <kristen.lee.3553@gmail.com>
2025-01-25 17:38:48 -0500

    made buttons into radio

---
+++ src/Components/ChoiceBubbleGroup.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import { SceneData } from "../Data/SceneData";
+import ChoiceBubble from "./ChoiceBubble";
+
+function ChoiceBubbleGroup({ sceneNum }) {
+    
+    const [selectedChoice, setSelectedChoice] = useState(null);
+
+    const handleChoiceSelect = (choiceNum) => {
+      setSelectedChoice(choiceNum); // Set the selected choice number
+    };
+
+    const choices = Object.values(SceneData[sceneNum].choices);
+  
+    return (
+        <div className="d-flex flex-row mb-3">
+        {choices.map((choice, index) => (
+            <ChoiceBubble
+            key={index}
+            sceneNum={sceneNum}
+            choiceNum={index}
+            isSelected={selectedChoice === index} // Pass the selected state
+            onSelect={handleChoiceSelect} // Pass the function to update selected state
+        />
+        ))}
+        </div>
+    );
+}
+
+export default ChoiceBubbleGroup
--- src/Routes/EgoChoices.js
+++ src/Routes/EgoChoices.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import { SceneData } from '../Data/SceneData';
 import ChoiceBubble from '../Components/ChoiceBubble';
 import { handleGemeniAPICall } from '../AI/Gemeni';
+import ChoiceBubbleGroup from '../Components/ChoiceBubbleGroup';
 
 function EgoChoices() {
 
@@ -40,12 +41,7 @@ function EgoChoices() {
             </div>
         {/* Choices Text*/}
         <div class="center-choicediv">
-          <table>
-            <th><div class="stagger"><ChoiceBubble sceneNum={sceneNum} choiceNum={"choice1"}/></div></th>
-            <th><ChoiceBubble sceneNum={sceneNum} choiceNum={"choice2"}/></th>
-            <th><ChoiceBubble sceneNum={sceneNum} choiceNum={"choice3"}/></th>
-            <th><ChoiceBubble sceneNum={sceneNum} choiceNum={"choice4"}/></th>
-          </table>
+          <ChoiceBubbleGroup sceneNum={sceneNum}></ChoiceBubbleGroup>
             </div>
             <button type='button' onClick={() => handleEvilClick()}>Change to evil</button>
         </div>
