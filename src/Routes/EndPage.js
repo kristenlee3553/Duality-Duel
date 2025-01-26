4a5f04ee4ada5c50e16dcd79d50123570ce38e5e
Kristen Lee <kristen.lee.3553@gmail.com>
2025-01-25 22:34:04 -0500

    end page done

--- src/Routes/EndPage.js
+++ src/Routes/EndPage.js
@@ -1,5 +1,6 @@
 import "../Styles/EndPage.css"
 import { useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 // Win = 6, Meh, = 1-5, Lose = 0
 function EndPage({ state }) {
@@ -26,9 +27,72 @@ function EndPage({ state }) {
         };
     }, [state]); // Re-run when the state changes
 
+
+    function GetFirstHalf() {
+        if (state === 0) {           
+            return "You have successfully "
+        }
+
+         else {
+            return "You " 
+         }
+    }
+
+    function GetRedText() {
+        if (state === 0) {
+            return "acknowledged your shadow"
+        }
+        else if (state === 1) {
+            return "almost "
+        }
+        else {
+            return "did not "
+        }
+    }
+
+    function GetLastHalf() {
+        if (state === 0) {
+            return "! The shadow embodies the hidden parts of yourself—traits, emotions, or desires often suppressed or denied. By recognizing it, you’ve taken a step toward greater self-awareness and a more balanced understanding of who you are."
+        }
+        else if (state === 1) {
+            return "fully acknowledged your shadow. At times, you may recognize your shadow, but other times, you may avoid confronting it. This inconsistency can cause emotional strain, as the parts of yourself you avoid continue to surface in unexpected ways."
+        }
+        else {
+            return "acknowledge your shadow. The shadow embodies the hidden parts of yourself—traits, emotions, or desires often suppressed or denied. By denying its existence, you risk projecting these qualities onto others, distorting your perceptions and relationships."
+        }
+    }
+
+    function GetHeaderText() {
+        if (state === 0) {
+         return "Success!"   
+        }
+        else if (state === 1) {
+            return "So Close!"
+        }
+        else {
+            return "Not Quite There Yet..."
+        }
+    }
+
+    function handlePlayAgain() {
+        window.location.reload(false);
+    }
+
     return (
         <div>
-            <h1>{state}</h1>
+            <div className="end-title">
+                <h1 className="header-text">{GetHeaderText()}</h1>
+            </div> 
+            <div className="end-description">
+                <h3 className="description-text">{GetFirstHalf()}
+                    <span  style={{color: "red"}}>{GetRedText()}</span>
+                    <span>{GetLastHalf()}</span>
+                </h3>
+            </div>
+            <button className='buttonPlayAgain' onClick={handlePlayAgain}>Play Again</button>
+            <Link to="/">
+                <button className='buttonExit'>Exit</button>
+            </Link>
         </div>
     );
 }
